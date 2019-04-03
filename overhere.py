23d931087945a3e06d7d38d3f91a8a2d9e91ede5
Sam Finton <spamhinting@gmail.com>
2019-04-03 18:00:53 -0300

    Pycharm reformatting / removed unnecessary imports

--- overhere.py
+++ overhere.py
@@ -1,9 +1,11 @@
 import sys
-from workflow import Workflow3, ICON_WEB, ICON_WARNING, web
+
 from pyicloud import PyiCloudService
 
+from workflow import Workflow3
 
-for line in open("apidata.txt","r").readlines():
+login_info = None
+for line in open("apidata.txt", "r").readlines():
     login_info = line.split()
 
 api = PyiCloudService(login_info[0], login_info[1])
@@ -15,9 +17,9 @@ def main(wf):
         for i, trusted_device in enumerate(trusted_devices):
             # print "  %s: %s" % (i, trusted_device.get('deviceName',
             # "SMS to %s" % trusted_device.get('phoneNumber')))
-            wf.add_item(title= trusted_device.get('deviceName'),
-            subtitle= 'Send iCloud verification code to this device', valid="Yes",
-            arg= trusted_device)
+            wf.add_item(title=trusted_device.get('deviceName'),
+                        subtitle='Send iCloud verification code to this device', valid="Yes",
+                        arg=trusted_device)
         wf.send_feedback()
 
         # wf.add_item('Need to verify with iCloud two-factor authentication',
@@ -30,12 +32,13 @@ def main(wf):
         device_list = api.devices._devices
 
         for device_id, device in device_list.iteritems():
-            device_name = str(device).split(":",1)[1]
+            device_name = str(device).split(":", 1)[1]
             device_hardware = str(device).split(":")[0]
-            it = wf.add_item(title= device_name,
-            subtitle= device_hardware, valid="Yes",
-            arg= device_id)
+            it = wf.add_item(title=device_name,
+                             subtitle=device_hardware, valid="Yes",
+                             arg=device_id)
         wf.send_feedback()
+
 
 if __name__ == "__main__":
     wf = Workflow3()
